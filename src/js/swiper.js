3dd6de6d42f43e3d57c81703e5d9e47bcc805468
Daniel Botero <danes.daesboar02@gmail.com>
2021-10-07 21:25:51 -0500

    cree el slider-header

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,14 +1,4 @@
-// // ANCHOR SWIPPER
 
-//   // core version + navigation, pagination modules:
-//   import Swiper, { Navigation, Pagination } from 'swiper';
-//   // import Swiper and modules styles
-//   import 'swiper/css';
-//   import 'swiper/css/navigation';
-//   import 'swiper/css/pagination';
-
-//   // configure Swiper to use modules
-//   Swiper.use([Navigation, Pagination]);
 
 // var swiper = new Swiper('.swiper-container', {
 //     //- cssMode: true,
@@ -34,3 +24,27 @@
 //     mousewheel: false,
 //     keyboard: true,
 // });
+var swiper = new Swiper('.swiper-header', {
+    //- cssMode: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+    //- freeMode: true;
+
+    loop: true,
+    //- autoplay: {
+    //-     delay: 2500,
+    //-     disableOnInteraction: false,
+    //- },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    mousewheel: false,
+    keyboard: true,
+});
