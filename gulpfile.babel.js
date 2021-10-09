4aea0c5b1b140dc92386a9987641f4d61db2a515
Daniel Botero <danes.daesboar02@gmail.com>
2021-10-09 17:58:56 -0500

    cree la configuracion de lista blanca de purgecss

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -101,11 +101,6 @@ gulp.task ('css', function () {
 	return gulp.src ('./src/scss/styles.scss')
 		.pipe (sass (). on ('error', sass.logError))
 		.pipe (postcss (procesadores))
-        // .pipe(
-        //     clean({
-        //       content: ['./public/*.html']
-        //     })
-        //   )    
 		.pipe (gulp.dest ('./public/css'));
 });
 
@@ -115,7 +110,11 @@ gulp.task('clean', () => {
       .src('./public/css/styles.css')
       .pipe(
         clean({
-          content: ['./public/*.html']
+          content: ['./public/*.html'],
+          css: ['./public/css/styles.css'],
+          safelist: ['hamburguer--simple', 'navbar-header--scroll', 'navbar-header--hide', 'navbar-header--hamburguer'],
+          FontFace: true,
+          variables: true
         })
       )
       .pipe(gulp.dest('./public/css'));
@@ -126,7 +125,7 @@ gulp.task('default', () => {
     gulp.watch('./src/pug/**/*.pug', gulp.series('pugIndex'));
     gulp.watch('./src/pug/**/*.pug', gulp.series('pugPages'));
     gulp.watch('./src/scss/**/*.scss', gulp.series('css'))
-    // gulp.watch('./src/scss/**/*.scss', gulp.series('clean'))
+    gulp.watch('./src/scss/**/*.scss', gulp.series('clean'))
     // gulp.watch('./src/*.html', gulp.series('html-min'))
     // gulp.watch('./src/pug/**/*.pug', gulp.series('rename'));
     // gulp.watch('./src/scss/**/*.scss', gulp.series('sass'))
