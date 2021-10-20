0f4b2e00822b3f3ef1f2f3c13e167d701bf3d7a7
Daniel Botero <danes.daesboar02@gmail.com>
2021-10-19 22:19:11 -0500

    segui trabajando en la seccion crear cuenta

--- src/js/user.js
+++ src/js/user.js
@@ -1,9 +1,14 @@
 const userButtonNavBar = document.getElementById("userButtonNavBar"),
     user = document.querySelector(".user"),
-    userButtonClose = document.getElementById("userButtonClose")
+    userButtonClose = document.getElementById("userButtonClose"),
+    singInLogIn = document.getElementById("singInLogIn"),
+    singIn = document.getElementById("singIn"),
+    logIn = document.getElementById("logIn")
+
+let userButtonGoBack = document.getElementById("userButtonGoBack")
 
 userButtonNavBar.addEventListener("click", () => {
-    user.classList.toggle('user--visible')
+    user.classList.toggle("user--visible")
     if(user.classList.contains("user--visible")){
         disableScroll()
     }else{
@@ -11,10 +16,21 @@ userButtonNavBar.addEventListener("click", () => {
     }
 })
 userButtonClose.addEventListener("click", () => {
-    user.classList.toggle('user--visible')
+    user.classList.toggle("user--visible")
     if(user.classList.contains("user--visible")){
         disableScroll()
     }else{
         enableScroll()
     }
 })
+singInLogIn.addEventListener("click", () => {
+    userButtonGoBack.classList.toggle("opacity")
+    singIn.classList.toggle("display-none")
+    logIn.classList.toggle("display-none")
+})
+userButtonGoBack.addEventListener("click", () => {
+    userButtonGoBack.classList.toggle("opacity")
+    singIn.classList.toggle("display-none")
+    logIn.classList.toggle("display-none")
+})
+
