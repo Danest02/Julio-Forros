890373709e58467f7ad27109545821db4ab1c5b3
Daniel Botero <danes.daesboar02@gmail.com>
2021-10-27 23:14:46 -0500

    termine el dark mode con local storage

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,26 +1,18 @@
-const buttonDarkMode = document.getElementById("buttonDarkMode"),
-    prefresDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
-let body = document.getElementById("body"),
-    buttonDarkModeIcon = document.getElementById("buttonDarkModeIcon"),
-    buttonDarkModeText = document.getElementById("buttonDarkModeText")
+let buttonDarkMode = document.getElementById("buttonDarkMode")
+let prefresDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+let body = document.getElementById("body")
+let buttonDarkModeIcon = document.getElementById("buttonDarkModeIcon")
+let buttonDarkModeText = document.getElementById("buttonDarkModeText")
 
 
-// buttonDarkModeText.style.width = "100"
-// console.log(getComputedStyle(document.documentElement).getPropertyValue('--width-button-dark-mode-text'))
-
-// let buttonDarkModeTextWidth = buttonDarkModeText.clientWidth + 'px'
-// console.log(buttonDarkModeTextWidth)
-// document.documentElement.style.setProperty('--width-button-dark-mode-text', buttonDarkModeTextWidth)
-// buttonDarkModeText.style.width = "0"
-// function widthTextModeDark() {
-//     console.log(buttonDarkModeTextWidth)
-//     let buttonDarkModeTextWidth = buttonDarkModeText.clientWidth + 'px'
-//     buttonDarkModeText.style.width = "0"
-//     document.documentElement.style.setProperty('--width-button-dark-mode-text', buttonDarkModeTextWidth)
-// }
+if (localStorage.getItem('mode') == 'dark') {
+    body.classList.toggle('dark-mode');
+} else if (localStorage.getItem('mode') == 'light') {
+    body.classList.toggle('light-mode');
+}
 
 function changeMode() {
-    
+
     if (buttonDarkModeIcon.innerHTML == "dark_mode") {
         buttonDarkModeText.innerHTML = "Modo Claro"
         buttonDarkModeIcon.innerHTML = "light_mode"
@@ -28,30 +20,25 @@ function changeMode() {
         buttonDarkModeText.innerHTML = "Modo Oscuro"
         buttonDarkModeIcon.innerHTML = "dark_mode"
     }
-    // buttonDarkModeText.style.width = "100%"
-    buttonDarkModeText.classList.add("width-100")
-    let buttonDarkModeTextWidth =  buttonDarkModeText.clientWidth + 'px'
-    buttonDarkModeText.classList.add("width-0")
-    console.log(buttonDarkModeTextWidth)
-    // buttonDarkModeText.style.width = "0"
-    document.documentElement.style.setProperty('--width-button-dark-mode-text', buttonDarkModeTextWidth)
 
 }
-changeMode()
+if (prefresDarkScheme.matches) {
+    changeMode()
+} 
 
 if (buttonDarkMode) {
-
     buttonDarkMode.addEventListener("click", () => {
         changeMode()
-        buttonDarkMode.classList.add("width-var")
+        let mode;
         if (prefresDarkScheme.matches) {
             body.classList.toggle("light-mode")
-            
+            mode = body.classList.contains('light-mode') ? 'light' : 'dark'
         } else {
             body.classList.toggle("dark-mode")
-
-
+            mode = body.classList.contains('dark-mode') ? 'dark' : 'light'
         }
+        localStorage.setItem('mode', mode)
 
     })
+
 }
