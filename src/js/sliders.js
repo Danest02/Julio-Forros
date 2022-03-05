e0abc3cf43d28536d407633a26fc116c9aa48739
Daniel Botero <danes.daesboar02@gmail.com>
2022-03-04 21:13:46 -0500

    voy a cambiar el slider a position absolute

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,23 +1,57 @@
-const $gridSliderNextButtom = document.getElementById("grid-slider__next-button"),
+const
+$gridSliderNextButtom = document.getElementById("grid-slider__next-button"),
 $gridSliderBackButtom = document.getElementById("grid-slider__back-button"),
+$gridSlider = document.getElementById("grid-slider"),
 $gridSliderSlides = document.getElementById("grid-slider__slides"),
 slideWidth = $gridSliderSlides.firstChild.clientWidth,
 numberOfColumns = Math.floor(parseInt(getComputedStyle(document.documentElement).getPropertyValue('--column')))
-console.log(numberOfColumns)
-$gridSliderNextButtom.addEventListener("click", (event) => {
+
+let gridSliderPressed = false
+let startX
+let x
+
+$gridSlider.addEventListener('mousedown', (e) => {
+    gridSliderPressed = true
+    $gridSlider.style.cursor = "grabbing"
+    startX = e.offsetX - $gridSliderSlides.offsetLeft
+    // startX = $gridSliderSlides.offsetLeft
+    console.log($gridSliderSlides.offsetLeft)
+    console.log(startX)
+})
+$gridSlider.addEventListener('mouseenter', (e) => {
+    $gridSlider.style.cursor = "grab"
+})
+$gridSlider.addEventListener('mouseleave', (e) => {
+    $gridSlider.style.cursor = "default"
+})
+$gridSlider.addEventListener('mouseup', (e) => {
+    $gridSlider.style.cursor = "grab"
+})
+window.addEventListener('mouseup', (e) => {
+    gridSliderPressed = false
+})
+$gridSlider.addEventListener('mousemove', (e) => {
+     if(!gridSliderPressed) return
+    //  console.log(e.target)
+     e.preventDefault()
+     x = e.offsetX
+    //  console.log(x)
+     $gridSliderSlides.scrollLeft = x - startX
+    //  console.log($gridSliderSlides.scrollLeft = x - startX)
+    // console.log(startX)
+
+
+})
+
+$gridSliderNextButtom.addEventListener("click", (e) => {
     if($gridSliderSlides.scrollLeft > $gridSliderSlides.offsetWidth ){
         $gridSliderSlides.scrollLeft = 0
     }
-    // if(numberOfColumns == 2){
-    //     $gridSliderSlides.scrollLeft += slideWidth * 2
-    //     console.log("oka")
-    // }else{
-    // }
-    // window.scrollTo( slideWidth, 100 )
     $gridSliderSlides.scrollLeft += slideWidth * numberOfColumns
     console.log($gridSliderSlides.scrollLeft)
 })
-$gridSliderBackButtom.addEventListener("click", (event) => {
+$gridSliderBackButtom.addEventListener("click", (e) => {
     $gridSliderSlides.scrollLeft -= slideWidth * numberOfColumns
 })
+
 
